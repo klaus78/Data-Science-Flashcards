d29622dfe1431415345b8b109cf1f72e208a0545
LAPTOP-EK4KGUT1 <claudio.varini@eu.averydennison.com>
2021-03-14 00:55:08 +0100

    adding dojos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 function showHideAnswer() {
-    var x = event.target.parentElement
+    var divAnswer = event.target.parentElement
             .getElementsByClassName('divAnswer')[0];
 
-    if (x.style.display == "block") {
-          x.style.display = "none";
-    } else {
-          x.style.display = "block";
-    }
+    var divCode = event.target.parentElement.getElementsByClassName('divCode')[0];
+    
+
+      if(divAnswer) {
+            if (divAnswer.style.display == "block") {
+                  divAnswer.style.display = "none";
+                  } else {
+                  divAnswer.style.display = "block";
+                  }
+      }
+
+    
+      if(divCode) {
+            if (divCode.style.display == "block") {
+                  divCode.style.display = "none";
+            } else {
+                  divCode.style.display = "block";
+            }
+      }
 }
