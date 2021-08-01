d2afef462d2c09d615c9e9511cc04cb0e99d0d2f
klaus78 <tagliole@libero.it>
2021-08-01 11:58:39 +0200

    each question is now a button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 function showHideAnswer() {
-    var divAnswer = event.target.parentElement
-            .getElementsByClassName('divAnswer')[0];
 
-    var divCode = event.target.parentElement.getElementsByClassName('divCode')[0];
-    
+      event.target.classList.toggle("active")
 
-      if(divAnswer) {
+      var divQuestion = event.target;
+      
+      // in case we clicked on a child element, we move back to the parent element
+      // in order to find the proper controls
+      while (divQuestion.className == '')
+            divQuestion = divQuestion.parentElement;
+
+
+      var divAnswer = divQuestion.parentElement.getElementsByClassName('divAnswer')[0];
+
+      var divCode = divQuestion.parentElement.getElementsByClassName('divCode')[0];
+
+      if (divAnswer) {
             if (divAnswer.style.display == "block") {
                   divAnswer.style.display = "none";
             } else {
@@ -13,8 +22,7 @@ function showHideAnswer() {
             }
       }
 
-    
-      if(divCode) {
+      if (divCode) {
             if (divCode.style.display == "block") {
                   divCode.style.display = "none";
             } else {
@@ -24,28 +32,27 @@ function showHideAnswer() {
 }
 
 function showHideMenus() {
-      
+
       event.target.classList.toggle("active")
 
       next = event.target.nextElementSibling;
-      
-      while(next && next.classList.contains("hideable_h3"))
-      {
-            if(next) {
+
+      while (next && next.classList.contains("hideable_h3")) {
+            if (next) {
                   showHideElement(next);
                   next = next.nextElementSibling;
             }
             else return;
       }
 
-      if(!next) {
+      if (!next) {
 
       }
-      
+
 }
 
 function showHideElement(elem) {
-      if(elem) {
+      if (elem) {
             if (elem.style.display == "block") {
                   elem.style.display = "none";
             } else {
