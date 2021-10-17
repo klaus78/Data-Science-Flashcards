441f554be8c29fb46de4051862e277d8fb8a8d5c
klaus78 <tagliole@libero.it>
2021-10-17 22:26:02 +0200

    header working on mobile?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,11 @@
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { handleWindowScroll() };
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function handleWindowScroll() {
+window.onload = function() {
       // Get the header
       header = document.getElementById("myHeader");
       
       // Get the offset position of the navbar
       sticky = header.offsetTop;
-      if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-      } else {
-            header.classList.remove("sticky");
-      }
+      header.classList.add("sticky");
 }
 
 
